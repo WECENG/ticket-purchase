a0272b7f35e590825fd81ba818ef1e4b08f53553
xiaowei <1437591651@qq.com>
2025-09-14 01:27:14 +0800

    feature(抢票脚本)1.适配最新大麦APP;2.优化抢票速度;3.添加修改信息的md

--- damai_appium/config.py
+++ damai_appium/config.py
@@ -6,26 +6,35 @@ __Description__ = "配置类"
 __Created__ = 2023/10/27 09:54
 """
 import json
+import re
 
 
 class Config:
-    def __init__(self, server_url, keyword, users, city, date, price, if_commit_order):
+    def __init__(self, server_url, keyword, users, city, date, price, price_index, if_commit_order):
         self.server_url = server_url
         self.keyword = keyword
         self.users = users
         self.city = city
         self.date = date
         self.price = price
+        self.price_index = price_index
         self.if_commit_order = if_commit_order
 
     @staticmethod
     def load_config():
-        with open('config.json', 'r', encoding='utf-8') as config_file:
-            config = json.load(config_file)
+        with open('config.jsonc', 'r', encoding='utf-8') as config_file:
+            content = config_file.read()
+            # 去掉 // 的注释
+            content = re.sub(r'//.*', '', content)
+            # content = re.sub(r'/\*[\s\S]*?\*/', '', content)
+            print(content)
+            config = json.loads(content)
+            # config = json.load(config_file)
         return Config(config['server_url'],
                       config['keyword'],
                       config['users'],
                       config['city'],
                       config['date'],
                       config['price'],
+                      config['price_index'],
                       config['if_commit_order'])
